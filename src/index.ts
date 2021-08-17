c894d7a0c4cc80d9de8b15b859eb33299250d8b7
jvb <vvimoink@gmail.com>
2021-08-17 14:18:29 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: jvb
+ * @LastEditors: jvb
+ * @email: tusktalk@163.com
+ * @github: https://github.com/tusktalk
+ * @Date: 2021-08-17 13:58:35
+ * @LastEditTime: 2021-08-17 13:58:36
+ * @motto: Hi Virtual Mode
+ * @Description: Modify here please
+ * @FilePath: \Development\UI\joint-ui-icons\src\index.ts
+ */
